c9b0e210fb20273b24b1db9a9c94b8496a48f05b
ahmady016 <ahmady09@gmail.com>
2018-06-17 00:02:55 +0200

    adding between validation function

--- src/Common/validations.js
+++ src/Common/validations.js
@@ -1,10 +1,10 @@
 // base function to test regular expressions
 const regex = (pattern,value) => pattern.test(value);
-// test for numeric values 
+// test for numeric values
 const numeric = (value) => regex(/^[0-9]+$/,value);
 // test for e-mail address
 const email = (value) => regex(/^([A-Za-z0-9_\-\.]{3,})+\@([A-Za-z0-9_\-\.{2,}])+\.([A-Za-z]{2,4})$/, value);
-// test on alphabet characters - and take lang to allow en or ar otherwise allow both 
+// test on alphabet characters - and take lang to allow en or ar otherwise allow both
 const alpha = (value, lang) => {
   const ar_alpha = /^([\u0600-\u06ff ]|[\u0750-\u077f ]|[\ufb50-\ufbc1 ]|[\ufbd3-\ufd3f ]|[\ufd50-\ufd8f ]|[\ufd92-\ufdc7 ]|[\ufe70-\ufefc ]|[\ufdf0-\ufdfd ])*$/g,
         en_alpha = /^[a-zA-Z ]+$/;
@@ -17,7 +17,7 @@ const alpha = (value, lang) => {
       return ( regex(en_alpha,value) || regex(ar_alpha,value) );
   }
 }
-// test for a web url 
+// test for a web url
 const url = function(value) {
   const __url = new RegExp("^" +
       // protocol identifier
@@ -28,7 +28,7 @@ const url = function(value) {
       "(?!(?:10|127)(?:\\.\\d{1,3}){3})" +
       "(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})" +
       "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" +
-      // IP address dotted notation octets // excludes loopback network 0.0.0.0 
+      // IP address dotted notation octets // excludes loopback network 0.0.0.0
       // excludes reserved space >= 224.0.0.0 // excludes network & broacast addresses
       // (first & last IP address of each class)
       "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" +
@@ -52,7 +52,7 @@ const url = function(value) {
 const password = (value, strong = true) => {
   const strong = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
   const medium = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
-  if(strong) 
+  if(strong)
     return regex(strong, value);
   return regex(medium, value);
 }
@@ -67,6 +67,24 @@ const inList = (value, list, key) => {
     return list.some(item => (key)? (value === item[key]) : (value === item) );
   return "not a list ...";
 }
+const between = ({ type, value, min, max }) => {
+  switch (type) {
+    case 'string':
+      return (value.length >= min && value.length <= max);
+    case 'number':
+      value = +value;
+      min = +min;
+      max = +max;
+      return (value >= min && value <= max);
+    case 'date':
+      value = value.toDate();
+      min = min.toDate();
+      max = max.toDate();
+      return (value >= min && value <= max);
+    default:
+      return 'not a valid type !!!';
+  }
+}
 
 export {
   alpha,
@@ -76,5 +94,6 @@ export {
   password,
   required,
   confirmed,
-  inList
+  inList,
+  between
 }
